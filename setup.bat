a0b5f105937d27f3b5751c0b808a6140e95f3dc1
Rabi Khan <26781039+itsrabikhan@users.noreply.github.com>
2025-01-13 23:53:48 -0700

    Polished setup.bat file.

--- setup.bat
+++ setup.bat
@@ -1,12 +1,13 @@
-echo "Installing required packages..."
+@echo off
+echo Installing required packages...
 pip install -r requirements.txt
-echo "Installation complete. If any errors occurred"
-if %ERRORLEVEL% != 0 (
-    echo "Command failed. Please ask for help."
-    exit /b %ERRORLEVEL%
+if %ERRORLEVEL%==0 (
+    echo All packages installed successfully.
+echo Creating startup script...
+echo python main.py > run.bat
+echo Startup script created.
+echo Setup complete. You can now run the program by running typing "run" into your terminal or clicking the run file in File Explorer!
+) else (
+    echo An error occurred while installing packages. Please try again or ask for help.
+    exit /b
 )
-echo "All packages installed successfully."
-echo "Creating startup script..."
-echo "python main.py" > run.bat
-echo "Startup script created."
-echo "Setup complete. You can now run the program by running typing ""run"" into your terminal or clicking the run file in File Explorer!"
